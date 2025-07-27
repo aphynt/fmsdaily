4926af5eaf31d289d2436ede97299c733139791b
ahmadfadillllah <ahmadfadillah502@gmail.com>
2025-07-28 07:55:15 +0800

    optimize login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,76 @@
-@include('layout.head', ['title' => 'Login'])
+
+<!--
+    Copyright (c) 2024 Ahmad Fadillah - IT SIMS.
+    All rights reserved. Unauthorized duplication is prohibited.
+    -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login - Daily Foreman</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
+    <meta name="theme-color" content="#001831">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description"
+        content="Digitalisasi laporan foreman untuk efisiensi alur proses dan peningkatan akurasi data">
+    <meta name="keywords" content="Laporan Harian Pengawas">
+    <meta name="author" content="FMS - PT. SIMS JAYA KALTIM">
+    <link rel="icon" href="{{ asset('dashboard/assets') }}/images/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('dashboard/assets') }}/css/style.css" id="main-style-link">
+    <script src="{{ asset('dashboard/assets') }}/js/tech-stack.js"></script>
+    <script src="{{ asset('dashboard/assets') }}/js/hak-akses.js"></script>
+    <link rel="stylesheet" href="{{ asset('dashboard/assets') }}/css/plugins/notifier.css">
+    {{-- SweetAlert --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</head>
+
+<body data-pc-preset="preset-1" data-pc-sidebar-caption="true" data-pc-layout="vertical" data-pc-direction="ltr"
+    data-pc-theme_contrast="" data-pc-theme="light">
+
+    @include('layout.alert.general')
+
+    <style>
+        .notifier-container {
+            position: fixed;
+            top: 20px;
+            right: 20px;
+            z-index: 9999;
+            background-color: #001831;
+            color: white;
+            padding: 15px;
+            border-radius: 5px;
+            display: none;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            transition: opacity 0.5s ease-in-out;
+            max-width: 90%;
+        }
+
+        .notifier-container.show {
+            display: block;
+            opacity: 1;
+        }
+
+        .notifier-container.hide {
+            opacity: 0;
+            display: block;
+        }
+
+        @media (max-width: 768px) {
+            .notifier-container {
+                top: 10px;
+                right: 10px;
+                font-size: 14px;
+                padding: 10px;
+            }
+        }
+    </style>
+
     <div class="auth-main">
         <div class="auth-wrapper v1">
             <div class="auth-form">
@@ -29,209 +101,8 @@
             </div>
         </div>
     </div><!-- [ Main Content ] end -->
-    <!-- Required Js -->
-    <script src="{{ asset('dashboard/assets') }}/js/plugins/popper.min.js"></script>
-    <script src="{{ asset('dashboard/assets') }}/js/plugins/simplebar.min.js"></script>
-    <script src="{{ asset('dashboard/assets') }}/js/plugins/bootstrap.min.js"></script>
-    <script src="{{ asset('dashboard/assets') }}/js/fonts/custom-font.js"></script>
     <script src="{{ asset('dashboard/assets') }}/js/pcoded.js"></script>
-    <script src="{{ asset('dashboard/assets') }}/js/plugins/feather.min.js"></script>
-    <script src="{{ asset('dashboard/assets') }}/js/plugins/notifier.js"></script>
-    <script src="{{ asset('dashboard/assets') }}/js/pages/ac-notification.js"></script>
-    <script>
-        layout_change('light');
 
-    </script>
-    <script>
-        change_box_container('false');
-
-    </script>
-    <script>
-        layout_caption_change('true');
-
-    </script>
-    <script>
-        layout_rtl_change('false');
-
-    </script>
-    <script>
-        preset_change('preset-1');
-
-    </script>
-    <script>
-        main_layout_change('vertical');
-
-    </script>
-    <div class="pct-c-btn"><a href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvas_pc_layout"><i
-                class="ph-duotone ph-gear-six"></i></a></div>
-    <div class="offcanvas border-0 pct-offcanvas offcanvas-end" tabindex="-1" id="offcanvas_pc_layout">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title">Settings</h5><button type="button" class="btn btn-icon btn-link-danger ms-auto"
-                data-bs-dismiss="offcanvas" aria-label="Close"><i class="ti ti-x"></i></button>
-        </div>
-        <div class="pct-body customizer-body">
-            <div class="offcanvas-body py-0">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <div class="pc-dark">
-                            <h6 class="mb-1">Theme Mode</h6>
-                            <p class="text-muted text-sm">Choose light or dark mode or Auto</p>
-                            <div class="row theme-color theme-layout">
-                                <div class="col-4">
-                                    <div class="d-grid"><button class="preset-btn btn active" data-value="true"
-                                            onclick="layout_change('light');" data-bs-toggle="tooltip"
-                                            title="Light"><svg class="pc-icon text-warning">
-                                                <use xlink:href="#custom-sun-1"></use>
-                                            </svg></button></div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="d-grid"><button class="preset-btn btn" data-value="false"
-                                            onclick="layout_change('dark');" data-bs-toggle="tooltip" title="Dark"><svg
-                                                class="pc-icon">
-                                                <use xlink:href="#custom-moon"></use>
-                                            </svg></button></div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="d-grid"><button class="preset-btn btn" data-value="default"
-                                            onclick="layout_change_default();" data-bs-toggle="tooltip"
-                                            title="Automatically sets the theme based on user's operating system's color scheme."><span
-                                                class="pc-lay-icon d-flex align-items-center justify-content-center"><i
-                                                    class="ph-duotone ph-cpu"></i></span></button></div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <h6 class="mb-1">Theme Contrast</h6>
-                        <p class="text-muted text-sm">Choose theme contrast</p>
-                        <div class="row theme-contrast">
-                            <div class="col-6">
-                                <div class="d-grid"><button class="preset-btn btn" data-value="true"
-                                        onclick="layout_theme_contrast_change('true');" data-bs-toggle="tooltip"
-                                        title="True"><svg class="pc-icon">
-                                            <use xlink:href="#custom-mask"></use>
-                                        </svg></button></div>
-                            </div>
-                            <div class="col-6">
-                                <div class="d-grid"><button class="preset-btn btn active" data-value="false"
-                                        onclick="layout_theme_contrast_change('false');" data-bs-toggle="tooltip"
-                                        title="False"><svg class="pc-icon">
-                                            <use xlink:href="#custom-mask-1-outline"></use>
-                                        </svg></button></div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <h6 class="mb-1">Custom Theme</h6>
-                        <p class="text-muted text-sm">Choose your primary theme color</p>
-                        <div class="theme-color preset-color"><a href="#!" data-bs-toggle="tooltip" title="Blue"
-                                class="active" data-value="preset-1"><i class="ti ti-checks"></i></a> <a href="#!"
-                                data-bs-toggle="tooltip" title="Indigo" data-value="preset-2"><i
-                                    class="ti ti-checks"></i></a> <a href="#!" data-bs-toggle="tooltip" title="Purple"
-                                data-value="preset-3"><i class="ti ti-checks"></i></a> <a href="#!"
-                                data-bs-toggle="tooltip" title="Pink" data-value="preset-4"><i
-                                    class="ti ti-checks"></i></a> <a href="#!" data-bs-toggle="tooltip" title="Red"
-                                data-value="preset-5"><i class="ti ti-checks"></i></a> <a href="#!"
-                                data-bs-toggle="tooltip" title="Orange" data-value="preset-6"><i
-                                    class="ti ti-checks"></i></a> <a href="#!" data-bs-toggle="tooltip" title="Yellow"
-                                data-value="preset-7"><i class="ti ti-checks"></i></a> <a href="#!"
-                                data-bs-toggle="tooltip" title="Green" data-value="preset-8"><i
-                                    class="ti ti-checks"></i></a> <a href="#!" data-bs-toggle="tooltip" title="Teal"
-                                data-value="preset-9"><i class="ti ti-checks"></i></a> <a href="#!"
-                                data-bs-toggle="tooltip" title="Cyan" data-value="preset-10"><i
-                                    class="ti ti-checks"></i></a></div>
-                    </li>
-                    <li class="list-group-item">
-                        <h6 class="mb-1">Theme layout</h6>
-                        <p class="text-muted text-sm">Choose your layout</p>
-                        <div class="theme-main-layout d-flex align-center gap-1 w-100"><a href="#!"
-                                data-bs-toggle="tooltip" title="Vertical" class="active" data-value="vertical"><img
-                                    src="{{ asset('dashboard/assets') }}/images/customizer/caption-on.svg" alt="img"
-                                    class="img-fluid"> </a><a href="#!" data-bs-toggle="tooltip" title="Horizontal"
-                                data-value="horizontal"><img
-                                    src="{{ asset('dashboard/assets') }}/images/customizer/horizontal.svg" alt="img"
-                                    class="img-fluid"> </a><a href="#!" data-bs-toggle="tooltip" title="Color Header"
-                                data-value="color-header"><img
-                                    src="{{ asset('dashboard/assets') }}/images/customizer/color-header.svg" alt="img"
-                                    class="img-fluid">
-                            </a><a href="#!" data-bs-toggle="tooltip" title="Compact" data-value="compact"><img
-                                    src="{{ asset('dashboard/assets') }}/images/customizer/compact.svg" alt="img"
-                                    class="img-fluid"> </a><a href="#!" data-bs-toggle="tooltip" title="Tab"
-                                data-value="tab"><img src="{{ asset('dashboard/assets') }}/images/customizer/tab.svg"
-                                    alt="img" class="img-fluid"></a></div>
-                    </li>
-                    <li class="list-group-item">
-                        <h6 class="mb-1">Sidebar Caption</h6>
-                        <p class="text-muted text-sm">Sidebar Caption Hide/Show</p>
-                        <div class="row theme-color theme-nav-caption">
-                            <div class="col-6">
-                                <div class="d-grid"><button class="preset-btn btn-img btn active" data-value="true"
-                                        onclick="layout_caption_change('true');" data-bs-toggle="tooltip"
-                                        title="Caption Show"><img
-                                            src="{{ asset('dashboard/assets') }}/images/customizer/caption-on.svg"
-                                            alt="img" class="img-fluid"></button></div>
-                            </div>
-                            <div class="col-6">
-                                <div class="d-grid"><button class="preset-btn btn-img btn" data-value="false"
-                                        onclick="layout_caption_change('false');" data-bs-toggle="tooltip"
-                                        title="Caption Hide"><img
-                                            src="{{ asset('dashboard/assets') }}/images/customizer/caption-off.svg"
-                                            alt="img" class="img-fluid"></button></div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="pc-rtl">
-                            <h6 class="mb-1">Theme Layout</h6>
-                            <p class="text-muted text-sm">LTR/RTL</p>
-                            <div class="row theme-color theme-direction">
-                                <div class="col-6">
-                                    <div class="d-grid"><button class="preset-btn btn-img btn active" data-value="false"
-                                            onclick="layout_rtl_change('false');" data-bs-toggle="tooltip"
-                                            title="LTR"><img
-                                                src="{{ asset('dashboard/assets') }}/images/customizer/ltr.svg"
-                                                alt="img" class="img-fluid"></button></div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="d-grid"><button class="preset-btn btn-img btn" data-value="true"
-                                            onclick="layout_rtl_change('true');" data-bs-toggle="tooltip"
-                                            title="RTL"><img
-                                                src="{{ asset('dashboard/assets') }}/images/customizer/rtl.svg"
-                                                alt="img" class="img-fluid"></button></div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item pc-box-width">
-                        <div class="pc-container-width">
-                            <h6 class="mb-1">Layout Width</h6>
-                            <p class="text-muted text-sm">Choose Full or Container Layout</p>
-                            <div class="row theme-color theme-container">
-                                <div class="col-6">
-                                    <div class="d-grid"><button class="preset-btn btn-img btn active" data-value="false"
-                                            onclick="change_box_container('false')" data-bs-toggle="tooltip"
-                                            title="Full Width"><img
-                                                src="{{ asset('dashboard/assets') }}/images/customizer/full.svg"
-                                                alt="img" class="img-fluid"></button></div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="d-grid"><button class="preset-btn btn-img btn" data-value="true"
-                                            onclick="change_box_container('true')" data-bs-toggle="tooltip"
-                                            title="Fixed Width"><img
-                                                src="{{ asset('dashboard/assets') }}/images/customizer/fixed.svg"
-                                                alt="img" class="img-fluid"></button></div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="d-grid"><button class="btn btn-light-danger" id="layoutreset">Reset Layout</button>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
 </body>
 
 </html>
