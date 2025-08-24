3e4d4604973e61f0b8d21ceec161306472ab50a7
ahmadfadillllah <ahmadfadillah502@gmail.com>
2025-08-24 16:49:56 +0800

    Update index.blade.php

--- resources/views/sop/produksi/index.blade.php
+++ resources/views/sop/produksi/index.blade.php
@@ -24,18 +24,34 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row">
-        <div class="col">
-            <div class="shadow-5-strong p-2">
-                <div class="embed-responsive embed-responsive-16by9 leb">
-                    <iframe src="https://docs.google.com/gview?url={{ asset('sop/perawatan_dan_penimbunan_jalan.pdf') }}&embedded=true" width="100%" frameborder="0" height="800px" style="border:none;"></iframe>
-                </div>
-            </div>
-        </div>
-    </div>
+                        <div class="col">
+                            <div class="shadow-5-strong p-2">
+                                <div class="embed-responsive embed-responsive-16by9 leb">
+                                    <iframe id="myIframe" src="" width="100%" frameborder="0" height="800px" style="border:none;"></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    function reloadAndLoadIframe() {
+        // Memeriksa apakah iframe sudah dimuat
+        if (!sessionStorage.getItem('iframeLoaded')) {
+            // Jika belum, reload halaman
+            sessionStorage.setItem('iframeLoaded', 'true');
+            location.reload();
+        } else {
+            // Jika sudah, muat iframe
+            document.getElementById('myIframe').src = 'https://docs.google.com/gview?url={{ asset('sop/perawatan_dan_penimbunan_jalan.pdf') }}&embedded=true';
+        }
+    }
+
+    window.onload = reloadAndLoadIframe;
+</script>
 
 @include('layout.footer')
