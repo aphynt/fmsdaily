0b714b086f856adcf662a33ea9c1e7acf3f4f545
ahmadfadillllah <ahmadfadillah502@gmail.com>
2025-09-24 17:53:26 +0800

    a

--- resources/views/job-pending/show.blade.php
+++ resources/views/job-pending/show.blade.php
@@ -94,8 +94,8 @@
                                 <p class="mb-0" style="white-space: pre-wrap; line-height: 1.6;">{!! nl2br(e($data[0]->catatan_verified_diterima)) !!}</p>
                             </div>
                             @endif
-                            @if ($data[0]->catatan_verified_diterima == null && $data[0]->verified_diterima == Auth::user()->nik)
-                            <form action="{{ route('jobpending.catatanPenerima', $data[0]->uuid) }}" method="get">
+                            {{-- @if ($data[0]->catatan_verified_diterima == null && $data[0]->verified_diterima == Auth::user()->nik)
+                            <form action="{{ route('jobpending.catatanPenerima', $data[0]->uuid) }}" method="post">
                                 @csrf
                                 <hr style="border: none; border-top: 2px dashed #000; height: 0; background: transparent;">
                                 <div class="col-md-12 col-12 px-2 py-2">
@@ -110,7 +110,7 @@
 
                             </form>
 
-                            @endif
+                            @endif --}}
 
                             <div class="col-sm-6">
                                 <div class="border rounded p-3">
